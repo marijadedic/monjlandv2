355c2e7fcf4fac525b83fcb156cd47b1ede78c13
Marija Dedic <marija.dedic@deptagency.com>
2022-08-17 17:06:08 +0200

    README and linting fixes

--- src/server.ts
+++ src/server.ts
@@ -4,13 +4,10 @@ dotenv.config();
 
 import Koa from 'koa';
 
-import bodyParser from 'koa-bodyparser';
-
 import http from 'http';
 
 const app = new Koa();
 
-app.use(bodyParser());
 
 const server = http.createServer(app.callback());
 
